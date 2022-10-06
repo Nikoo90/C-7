a80a48319a3516a7ad0b927e6b43fa8977fbe145
Nikoo90 <66570980+Nikoo90@users.noreply.github.com>
2022-10-06 23:20:12 +0300

    find number in array

---
+++ 2/Program.cs
@@ -0,0 +1,46 @@
+﻿
+int[,] ArrayTwoDimensional(int row, int col, int start, int end)
+{
+
+    int[,] arr = new int[row, col];
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            arr[i, j] = new Random().Next(start, end + 1);
+
+
+        }
+    }
+    return arr;
+}
+
+void PrintArrayTwoDimensional(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write($"{arr[i, j]}    ");
+        }
+        Console.WriteLine();
+    }
+}
+
+bool FindNumberInArray(int[,] arr, int num)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            if (arr[i, j] == num) return true;
+        }
+    }
+    return false;
+}
+
+int[,] arr = ArrayTwoDimensional(3, 4, 1, 100);
+
+PrintArrayTwoDimensional(arr);
+
+Console.WriteLine(FindNumberInArray(arr, 100));
