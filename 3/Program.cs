2ee5a90c54c9b1a86ea51107db4e4517831369ca
Nikoo90 <66570980+Nikoo90@users.noreply.github.com>
2022-10-07 00:34:36 +0300

    Find the arithmetic mean of the elements in each column

---
+++ 3/Program.cs
@@ -0,0 +1,51 @@
+﻿int[,] ArrayTwoDimensional(int row, int col, int start, int end)
+{
+
+    int[,] arr = new int[row, col];
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            arr[i, j] = new Random().Next(start, end + 1);
+
+
+        }
+    }
+    return arr;
+}
+
+void PrintArrayTwoDimensional(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write($"{arr[i, j]}    ");
+        }
+        Console.WriteLine();
+    }
+}
+
+double[] AverageColumnArray(int[,] arr)
+{
+
+    double[] result = new double[arr.GetLength(1)];
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            result[j] += Math.Round(Convert.ToDouble(arr[i, j]) / arr.GetLength(0), 2);
+        }
+
+    }
+
+    return result;
+}
+
+int[,] arr = ArrayTwoDimensional(3, 4, 1, 100);
+PrintArrayTwoDimensional(arr);
+Console.WriteLine();
+double[] arr1 = AverageColumnArray(arr);
+for (int i = 0; i < arr1.Length; i++) Console.Write($"{i + 1}){arr1[i]}  ");
+Console.WriteLine();
